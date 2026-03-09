aaef07505155f97a711e6d140ef895b441686b58
NamTM-24 <hnt0024.exe@gmail.com>
2026-03-10 02:46:36 +0700

    update

--- test/promise.js
+++ test/promise.js
@@ -1,13 +1,9 @@
-const myFirstPromise = new Promise((resolve, reject) => {
-  // We call resolve(...) when what we were doing asynchronously
-  // was successful, and reject(...) when it failed.
+const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Success!"); // Yay! Everything went well!
-  }, 250);
+    resolve("Done!");
+  }, 2000);
 });
 
-myFirstPromise.then((successMessage) => {
-  // successMessage is whatever we passed in the resolve(...) function above.
-  // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
-  console.log(`Yay! ${successMessage}`);
+promise.then((result) => {
+  console.log(result);
 });
--- test/test.js
+++ test/test.js
@@ -1,7 +1,29 @@
-function getURL(userInput) {
-  const regex = /\b[\w.-]+\.[a-z]{2,}\b/g;
-  const res = userInput.match(regex);
-  return res;
+class TranslationService {
+  constructor(api) {
+    this.api = api;
+  }
+
+  free(text) {
+    return this.api.fetch(text);
+  }
 }
-getURL("I learned a lot from exercism.org");
-// => ["exercism.org"];
+
+const api = {
+  data: {
+    hello: "xin chào",
+  },
+
+  fetch(text) {
+    return new Promise((resolve, reject) => {
+      if (this.data[text]) {
+        resolve(this.data[text]);
+      } else {
+        reject(new Error("Not yet translated"));
+      }
+    });
+  },
+};
+
+const service = new TranslationService(api);
+
+console.log(service.free("hello"));
